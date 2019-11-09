d4f50e3cf7841235efd960d7f06a26102b26442f
RookTKO <olaf.a.minkowicz@gmail.com>
2019-11-08 19:32:48 -0800

    Updated calc model

--- VAHomeVALoan/Models/Calculations.cs
+++ VAHomeVALoan/Models/Calculations.cs
@@ -7,5 +7,14 @@ namespace VAHomeVALoan.Models
 {
     public class Calculations
     {
+        public double HousePrice { get; set; }
+        public double DownPayment { get; set; }
+        public double LoanPeriod { get; set; }
+        public double InterestRate { get; set; }
+        public double HomeInsurace { get; set; }
+        public double PropertyTax { get; set; }
+        public double HOA { get; set; }
+        public double ZipCode { get; set; }
+        public double MonthlyPayment { get; set; }
     }
 }
